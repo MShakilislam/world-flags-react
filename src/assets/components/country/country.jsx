5347e3dbb4eb1975b5762b7beb5f0d7fb6d82d48
MShakilislam <shakilkpicse@gmail.com>
2025-10-17 01:10:25 +0600

    react condition

--- src/assets/components/country/country.jsx
+++ src/assets/components/country/country.jsx
@@ -1,14 +1,32 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './country.css'
 
 const Country = ({country}) => {
-    console.log(country.area.area)
+    const [Visited ,setVisited] = useState(false)
+    // console.log(country.area.area)
+
+    const halndleClicked =() =>{
+        // basyic syste 
+        // if(Visited){
+        //     setVisited(false)
+        // }
+        // else{
+        //     setVisited(true)
+        // }
+        // 2 secound system 
+        // setVisited(Visited ? false : true)
+        // 3 third system 
+        setVisited(!Visited)
+
+    }
+
     return (
-        <div className='country'>
+        <div className={`country  ${Visited && "country-visited"}`}>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h2>Name : {country.name.common}</h2>
             <h2>Population : {country.population.population}</h2>
             <p>Area : {country.area.area} {country.area.area > 300000 ? "Big Country" :"Small Country"}</p>
+            <button onClick={halndleClicked}>{Visited ? "Visited" : "Not Visited"}</button>
         </div>
     );
 };
