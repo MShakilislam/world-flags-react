0709726ca917fe9e12c4a507e92ffd508836b214
MShakilislam <shakilkpicse@gmail.com>
2025-10-17 00:38:47 +0600

    secound commit

--- src/assets/components/countres/countres.jsx
+++ src/assets/components/countres/countres.jsx
@@ -1,16 +1,19 @@
 import React, { use } from 'react';
 import Country from '../country/country';
+import './countres.css'
 
-const Countres = ({fetchPromis}) => {
+const Countres = ({ fetchPromis }) => {
     const countresData = use(fetchPromis)
     const countres = countresData.countries
     // console.log(countres)
     return (
         <div>
             <h3>I am a good boy : {countres.length}</h3>
-           {
-            countres.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
-           }
+            <div className='countres'>
+                {
+                    countres.map(country => <Country key={country.cca3.cca3} country={country}></Country>)
+                }
+            </div>
         </div>
     );
 };
--- src/assets/components/country/country.jsx
+++ src/assets/components/country/country.jsx
@@ -1,12 +1,14 @@
 import React from 'react';
+import './country.css'
 
 const Country = ({country}) => {
-    // console.log(country.cca3.cca3)
+    console.log(country.area.area)
     return (
-        <div>
+        <div className='country'>
             <img src={country.flags.flags.png} alt={country.flags.flags.alt} />
             <h2>Name : {country.name.common}</h2>
             <h2>Population : {country.population.population}</h2>
+            <p>Area : {country.area.area} {country.area.area > 300000 ? "Big Country" :"Small Country"}</p>
         </div>
     );
 };
